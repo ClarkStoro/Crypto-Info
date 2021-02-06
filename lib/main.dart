5ce190c86e1a7984ebd04a13782aeddd41c5af61
Emanuele Maso <emanuele.maso97@gmail.com>
2021-02-06 18:14:18 +0100

    Show list of cryptos

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dto/CoinCapResponse.dart';
+import 'dto/Currency.dart';
 
 
 void main() {
@@ -58,6 +59,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String _name;
+  List<Currency> _currencies = List.empty();
 
   Future<CoinCapResponse> _fetchCurrency() async {
     final response = await http.get('https://api.coincap.io/v2/assets/');
@@ -68,6 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
       CoinCapResponse res = CoinCapResponse.fromJson(response.body);
       setState(() {
           _name = res.data[0].name;
+
+          _currencies = res.data;
       });
       return res;
 
@@ -78,16 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -103,39 +97,15 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_name',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: ListView.builder(
+        itemCount: _currencies.length,
+        itemBuilder: (context, index) {
+            return ListTile(title: Text(_currencies[index].name));
+          },
         ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _fetchCurrency,
-        tooltip: 'Increment',
+        tooltip: 'Get data',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
